06e435010189ff0b000543a0dadc4bb1c441f7b1
Dani <danielporras147@outlook.es>
2023-01-30 18:56:18 +0100

    Ejercicio 03

---
+++ PedirDatosUsuario/Ejercicio03/ejercicio03.js
@@ -0,0 +1,8 @@
+/**
+ * 3 - Crea una página web que pida al usuario dos números y escriba su diferencia
+ */
+
+let n1 = +prompt("Introduzca un número: ");
+let n2 = +prompt("Introduzca otro número: ");
+
+document.write("La direfencia entre " + n1 + " - " + n2 + " es : " +(n1-n2));
