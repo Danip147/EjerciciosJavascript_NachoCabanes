07f0613b5358bb16cb86d6f6f076791a52d6d46a
Dani <danielporras147@outlook.es>
2023-01-30 18:55:59 +0100

    Ejercicio 02

---
+++ PedirDatosUsuario/Ejercicio02/ejercicio02.js
@@ -0,0 +1,8 @@
+/**
+ * 2 - Crea una página web que pida al usuario dos números y escriba su multiplicación
+ */
+
+let n1 = +prompt("Introduzca un número: ");
+let n2 = +prompt("Introduzca otro número: ");
+
+document.write("El resultado de multiplicar " + n1 + " X " + n2 + " es : " +(n1*n2));
