989578bd4b19ff4569754131d2d1def601ebf2c8
Dani <danielporras147@outlook.es>
2023-01-30 18:56:38 +0100

    Ejercicio 04

---
+++ PedirDatosUsuario/Ejercicio04/ejercicio04.js
@@ -0,0 +1,8 @@
+/**
+ * 4 - Crea una página web que pida al usuario dos números y escriba su división
+ */
+
+let n1 = +prompt("Introduzca un número: ");
+let n2 = +prompt("Introduzca otro número: ");
+
+document.write("El resultado de dividir " + n1 + " / " + n2 + " es : " +(n1/n2));
